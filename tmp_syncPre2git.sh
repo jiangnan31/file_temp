22420306e87ab00a3dfe7db665db2195de3e7688
houpu <houpu@mogu.com>
2022-04-07 16:36:14 +0800

    @houpu, stash pre jars

---
+++ tmp_syncPre2git.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+APP_HOME=$(cd "$(dirname "$0")"; pwd)
+SOURCE_HOME=/Users/jiangnan/Work/github_mogu/taichi
+
+cd $SOURCE_HOME
+mvn clean compile package  -Dmaven.test.skip=true -Ppre
+
+cd $APP_HOME
+cp $SOURCE_HOME/target/taichi-server.tar.gz ./
+tar -zxvf taichi-server.tar.gz
+rm -rf taichi-server/lib/libtensorflow_jni-1.15.0.jar
+rm -rf taichi-server/lib/stanford-corenlp-3.6.0-models.jar
+rm -rf taichi-server.tar.gz
+
