cb8c44269a3cdcef3aec25238efcf8e62ef2f3c4
houpu <houpu@mogu.com>
2022-04-12 14:20:54 +0800

    @houpu, update pre

--- syncPre2git.sh
+++ syncPre2git.sh
@@ -3,8 +3,8 @@
 APP_HOME=$(cd "$(dirname "$0")"; pwd)
 SOURCE_HOME=/Users/jiangnan/Work/github_mogu/taichi
 
-#cd $SOURCE_HOME
-#mvn clean compile package  -Dmaven.test.skip=true -Ppre
+cd $SOURCE_HOME
+mvn clean compile package  -Dmaven.test.skip=true -Ppre
 
 cd $APP_HOME
 cp -rf $SOURCE_HOME/target/taichi-server.tar.gz ./
