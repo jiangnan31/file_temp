a6429eb495b2b18ce858c453dc1c6b42f99b3552
houpu <houpu@mogu.com>
2022-12-21 17:48:10 +0800

    @houpu,update

---
+++ syncFromGit_pre.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+APP_HOME=$(cd "$(dirname "$0")"; pwd)
+GIT_SOURCE_HOME=/home/mapp/github_file_temp
+
+TARGET_HOME=/home/mapp/taichi-server
+
+git pull
+rm -rf $TARGET_HOME/lib/*
+cp -rf ./taichi-server-pre/* $TARGET_HOME
+cp -rf $TARGET_HOME/lib_extend/* $TARGET_HOME/lib/
